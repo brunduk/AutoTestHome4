f3654b83c1b63a129dae5159f115f56804b8d86b
double d <brunduk4415@gmail.com>
2023-07-25 17:07:50 +0300

    update

--- src/test/java/ru/netology/DeliveryCardTest.java
+++ src/test/java/ru/netology/DeliveryCardTest.java
@@ -4,6 +4,7 @@ import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.SelenideElement;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
 
 import java.time.Duration;
@@ -25,50 +26,56 @@ public class DeliveryCardTest {
         open("http://localhost:9999/");
     }
 
-    DateTimeFormatter format = DateTimeFormatter.ofPattern("dd-MM-yyyy");
-    String date = format.format(LocalDate.now().plusDays(3));
+    //DateTimeFormatter format = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+    //String date = format.format(LocalDate.now().plusDays(3));
+    public String date(long addDays, String pattern) {
+        return LocalDate.now().plusDays(addDays).format(DateTimeFormatter.ofPattern(pattern));
+    }
+    String planningDate = date(3, "dd.MM.yyyy");
 
     @Test
     void OrderDeliveryCard() {
 
         $("[data-test-id='city'] input").setValue("Уфа");
-        $("[data-test-id='date'] input").doubleClick().press("backspace");
-        $("[data-test-id='date'] input").setValue(date);
+        $("[data-test-id='date'] input").doubleClick().press(Keys.BACK_SPACE);
+        $("[data-test-id='date'] input").setValue(planningDate);
         $("[data-test-id='name'] input").setValue("Иванов Иван");
         $("[data-test-id='phone'] input").setValue("+79211234567");
         $(".checkbox__box").click();
         $(".button").click();
         $(byText("Успешно!")).shouldBe(Condition.visible, Duration.ofSeconds(15));
+        $(".notification__content").shouldHave(Condition.text("Встреча успешно забронирована на " + planningDate), Duration.ofSeconds(15))
+                .shouldBe(Condition.visible);
 
     }
 
-    @Test
-    void OrderDeliveryCardChangeCity() {
+ //   @Test
+  //  void OrderDeliveryCardChangeCity() {
 
-        $("[data-test-id='city'] input").setValue("Ка");
-        $$(".menu-item__control").find(exactText("Калининград")).click();
-        $("[data-test-id='date'] input").doubleClick().press("backspace");
-        $("[data-test-id='date'] input").setValue(date);
-        $("[data-test-id='name'] input").setValue("Иванов Иван");
-        $("[data-test-id='phone'] input").setValue("+79211234567");
-        $(".checkbox__box").click();
-        $(".button").click();
-        $(byText("Успешно!")).shouldBe(Condition.visible, Duration.ofSeconds(15));
+ //      $("[data-test-id='city'] input").setValue("Ка");
+  //      $$(".menu-item__control").find(exactText("Калининград")).click();
+  //      $("[data-test-id='date'] input").doubleClick().press(Keys.BACK_SPACE);
+  //      $("[data-test-id='date'] input").setValue(planningDate);
+  //      $("[data-test-id='name'] input").setValue("Иванов Иван");
+  //      $("[data-test-id='phone'] input").setValue("+79211234567");
+  //      $(".checkbox__box").click();
+  //      $(".button").click();
+  //      $(byText("Успешно!")).shouldBe(Condition.visible, Duration.ofSeconds(15));
 
-    }
+  //  }
 
-    @Test
-    void OrderDeliveryCardChangeCalendar() {
+  //  @Test
+  //  void OrderDeliveryCardChangeCalendar() {
 
-        $("[data-test-id='city'] input").setValue("Уфа");
-        $(".icon-button__text").click();
-        $$(".calendar__arrow_direction_right").last().click();
-        $(byText("3")).click();
-        $("[data-test-id='name'] input").setValue("Иванов Иван");
-        $("[data-test-id='phone'] input").setValue("+79211234567");
-        $(".checkbox__box").click();
-        $(".button").click();
-        $(byText("Успешно!")).shouldBe(Condition.visible, Duration.ofSeconds(15));
+ //       $("[data-test-id='city'] input").setValue("Уфа");
+  //      $(".icon-button__text").click();
+ //       $$(".calendar__arrow_direction_right").last().click();
+ //       $(byText("3")).click();
+ //       $("[data-test-id='name'] input").setValue("Иванов Иван");
+ //       $("[data-test-id='phone'] input").setValue("+79211234567");
+ //       $(".checkbox__box").click();
+  //      $(".button").click();
+ //       $(byText("Успешно!")).shouldBe(Condition.visible, Duration.ofSeconds(15));
 
-    }
+  //  }
 }
